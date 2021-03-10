66631a399206200f819937a9c5bf65d0c5ca8d06
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-09 19:54:58 -0500

    improved number check and removed useless sort

--- timer1.js
+++ timer1.js
@@ -1,4 +1,4 @@
-const arr = process.argv.slice(2).sort((a, b) => a - b).map(x => Number(x));
+const arr = process.argv.slice(2).map(x => Number(x));
 for (let num of arr) {
-  if (num > 0 && typeof num === 'number') setTimeout(() => console.log('beep!'), num * 1000);
+  if (num > 0 && typeof num === 'number' && isFinite(num)) setTimeout(() => console.log('beep!'), num * 1000);
 }
